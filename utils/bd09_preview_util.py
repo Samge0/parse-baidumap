b70bff29e1cd195eaaca6033a719fb4c59ae35de
shaochengbao <samge720@163.com>
2026-02-02 16:39:17 +0800

    更新：百度地图的预览改版

--- utils/bd09_preview_util.py
+++ utils/bd09_preview_util.py
@@ -9,19 +9,22 @@ MAP_TEMPLATE = """
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>百度地图围栏展示-BD09</title>
     <style>
         html, body {
             margin: 0;
             padding: 0;
-            height: 100%;  /* 确保父容器占满整个浏览器 */
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
         }
         #map {
-            width: 100%;   /* 宽度占满整个容器 */
-            height: 100vh; /* 高度占满视口 */
+            width: 100%;
+            height: 100%;
         }
     </style>
-    <script type="text/javascript" src="https://api.map.baidu.com/api?v=3.0&ak=TAG_APP_KEY"></script>
+    <script type="text/javascript" src="https://api.map.baidu.com/api?v=1.0&type=webgl&ak=TAG_APP_KEY"></script>
 </head>
 <body>
 
@@ -35,46 +38,62 @@ MAP_TEMPLATE = """
     var fenceData = JSON.parse(jsonString);
 
     // 初始化地图，设置中心点和缩放级别
-    var map = new BMap.Map("map");
+    var map = new BMapGL.Map("map");
+    var center, marker;
+
     if (fenceData.features[0].properties.center && fenceData.features[0].properties.center.length === 2) {
         // 有中心点
-        var center = new BMap.Point(fenceData.features[0].properties.center[0], fenceData.features[0].properties.center[1]);
+        center = new BMapGL.Point(fenceData.features[0].properties.center[0], fenceData.features[0].properties.center[1]);
         map.centerAndZoom(center, 18);
         // 在中心点添加一个标记
-        var marker = new BMap.Marker(center);
+        marker = new BMapGL.Marker(center);
         map.addOverlay(marker);
     } else {
         // 没有中心点则使用第一个多边形的第一个点
         var point = fenceData.features[0].geometry.coordinates[0][0];
-        map.centerAndZoom(new BMap.Point(point[0], point[1]), 18);
+        center = new BMapGL.Point(point[0], point[1]);
+        map.centerAndZoom(center, 18);
     }
 
     // 启用鼠标滚轮缩放
     map.enableScrollWheelZoom();
-    
+
+    // 设置地图类型为普通地图
+    map.setMapType(BMAP_NORMAL_MAP);
+
+    // 添加地图控件
+    map.addControl(new BMapGL.NavigationControl());
+    map.addControl(new BMapGL.ScaleControl());
+
     // 获取围栏的坐标 - 这里默认只处理一个多边形的情况，可以根据实际情况进行修改
     var fenceCoordinates = fenceData.features[0].geometry.coordinates[0];
 
     // 将围栏数据转为百度地图的坐标点
     var fencePath = fenceCoordinates.map(function(coord) {
-        return new BMap.Point(coord[0], coord[1]);
+        return new BMapGL.Point(coord[0], coord[1]);
     });
 
     // 绘制多边形（围栏）
-    var fencePolygon = new BMap.Polygon(fencePath, {
+    var fencePolygon = new BMapGL.Polygon(fencePath, {
         strokeColor: "blue",
         strokeWeight: 2,
         strokeOpacity: 0.5,
-        fillColor: "blue",
+        fillColor: "#4285f4",
         fillOpacity: 0.3
     });
     map.addOverlay(fencePolygon);
 
-    // 添加提示框
-    var infoWindow = new BMap.InfoWindow(fenceData.features[0].properties.name||"", { width: 200, height: 100, title: "围栏中心" });
-    marker.addEventListener("click", function() {
-        this.openInfoWindow(infoWindow);
-    });
+    // 添加提示框（如果有marker）
+    if (marker && fenceData.features[0].properties.name) {
+        var infoWindow = new BMapGL.InfoWindow(fenceData.features[0].properties.name, {
+            width: 200,
+            height: 80,
+            title: "围栏信息"
+        });
+        marker.addEventListener("click", function() {
+            map.openInfoWindow(infoWindow, center);
+        });
+    }
 </script>
 
 </body>
